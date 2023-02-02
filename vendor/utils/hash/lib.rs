7b172ae3dacbd50c2dfa89c56136ab6ba771f220
Jason Tatton <jat@meta.com>
2023-02-02 13:57:54 -0800

    vendor in dependencies from hphp
    Summary: Here we vendor in the dependencies ERRPY has on the Rust<->OCaml FFI provided by the Hack team under `/hphp/`. Note that we adjust the autocargo config so as to permit the automatic generation of the `Cargo.toml` files. Both the buck2 and cargo build now work ok
    
    Reviewed By: grievejia
    
    Differential Revision:
    D42970729
    
    Privacy Context Container: L1152058
    
    fbshipit-source-id: dab0b8b9e0348c25b277725d994163f1709f8994

---
+++ vendor/utils/hash/lib.rs
@@ -0,0 +1,10 @@
+// Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+
+pub type Hasher = rustc_hash::FxHasher;
+pub type BuildHasher = std::hash::BuildHasherDefault<Hasher>;
+pub type HashMap<K, V> = rustc_hash::FxHashMap<K, V>;
+pub type HashSet<K> = rustc_hash::FxHashSet<K>;
+pub type IndexMap<K, V> = indexmap::map::IndexMap<K, V, BuildHasher>;
+pub type IndexSet<K> = indexmap::set::IndexSet<K, BuildHasher>;
+pub type DashMap<K, V> = dashmap::DashMap<K, V, BuildHasher>;
+pub type DashSet<K> = dashmap::DashSet<K, BuildHasher>;
