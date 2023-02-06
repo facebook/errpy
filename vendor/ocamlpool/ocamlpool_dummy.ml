a02e05def816e8912cb2c848a2261d3390c67943
Jason Tatton <jat@meta.com>
2023-02-06 13:55:56 -0800

    add copyright headers
    Summary: add copyright header to `errpy.ml` and contents of `vendor/ocamlpool`
    
    Reviewed By: alexkassil
    
    Differential Revision: D43045853
    
    fbshipit-source-id: 34b0a751935734a4fa65eaa3e27257eec74d3f56

--- vendor/ocamlpool/ocamlpool_dummy.ml
+++ vendor/ocamlpool/ocamlpool_dummy.ml
@@ -1,4 +1,12 @@
 (*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ *)
+
+(*
  * From ocaml 4.12, libraries with any .ml modules will no longer
  * generates libfoo.a files.
  * Buck v1 is still expecting these, so the easiest workaround until
